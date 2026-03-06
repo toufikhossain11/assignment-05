f659aa186895a3f0d078f26df1f3f28a337cbd2a
toufikhossain11 <toufikhossain546@gmail.com>
2026-03-07 00:21:26 +0600

    3rd commit

---
+++ login.js
@@ -0,0 +1,18 @@
+
+
+// document.getElementById("login-btn").addEventListener('click',function(){
+    
+// })
+document.getElementById('login-btn').addEventListener('click',function(){
+    const username = document.getElementById('username');
+    const getUsername = username.value;
+    const password = document.getElementById('password');
+    const getPassword = password.value;
+    console.log(getPassword,getUsername);
+    if(getUsername === 'admin' && getPassword === 'admin123'){
+        window.location.assign('./index2.html')
+    }else{
+        alert('Password Worng');
+        return;
+    }
+})
